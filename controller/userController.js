38293f59394542bab7a81cd0b8c50e85fbbbcfbf
Saptak10 <saptak.bugatti20@gmail.com>
2022-08-01 13:17:40 +0530

    minor changes

--- controller/userController.js
+++ controller/userController.js
@@ -3,8 +3,10 @@ const asyncHandler = require('express-async-handler')
 
 module.exports.registerUser = asyncHandler(async(req,res) => {
     res.status(200);
+    // throw new Error('Please add a text field')
 })
 
 module.exports.loginUser = asyncHandler(async(req,res) => {
     res.status(200);
+    // throw new Error('Please add a text field')
 })

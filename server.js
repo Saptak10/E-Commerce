ace53a169a91e4b26a414226c67b5f50c1e023ac
Saptak10 <saptak.bugatti20@gmail.com>
2022-08-02 17:31:05 +0530

    graphql

--- server.js
+++ server.js
@@ -2,13 +2,16 @@ if (process.env.NODE_ENV !== "production") {
     require('dotenv').config();
 }
 
-const express = require('express')
-const path = require('path')
-const dotenv = require('dotenv')
+const express = require('express');
+const path = require('path');
+const dotenv = require('dotenv');
+const { graphqlHTTP } = require('express-graphql');
 
 const dbConnection = require('./config/dbConnection');
 const errorHandler = require('./middleware/errorMidlleware')
 const userRoute = require('./route/userRoutes');
+
+const schema = require('./schema/schema.js');
 
 const app = express();
 
@@ -16,6 +19,11 @@ dbConnection();
 
 app.use(express.json())
 app.use(express.urlencoded({ extended : true }))
+
+app.use('/graphql', graphqlHTTP({
+    schema,
+    graphiql : process.env.NODE_ENV === 'development'
+}))
 
 app.use('/', userRoute)
 

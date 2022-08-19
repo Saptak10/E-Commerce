eb473a3b9573f0aad98ab3c7adb76f0f4d3a9840
Saptak10 <saptak.bugatti20@gmail.com>
2022-08-20 01:08:35 +0530

    Frontend files added

--- client/src/App.js
+++ client/src/App.js
@@ -4,9 +4,9 @@ import { ToastContainer } from 'react-toastify'
 import Header from './components/Header';
 import Footer from './components/Footer';
 import Home from './components/Home';
-import Cart from './components/Cart';
 import Login from './components/Login';
 import Register from './components/Register';
+import Cart from './components/Cart/CartWidget';
 
 function App() {
   return (
---
+++ client/src/store/store.js
@@ -0,0 +1,12 @@
+import { configureStore } from '@reduxjs/toolkit'
+// import userReducer from '../features/users/userSlice'
+// import productReducer from '../features/products/productSlice'
+// import cartReducer from '../features/cart/cartSlice'
+
+export const store = configureStore({
+  reducer: {
+    // users: userReducer,
+    // products: productReducer,
+    // cart: cartReducer,
+  },
+})

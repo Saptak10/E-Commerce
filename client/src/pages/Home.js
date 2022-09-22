6965318dd4983f0ae4cd1a7b4dd5606c1380f511
Saptak10 <saptak.bugatti20@gmail.com>
2022-09-22 12:33:44 +0530

    bug fix

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,9 +1,9 @@
-import Categories from "../components/Categories/Categories";
+// import Categories from "../components/Categories/Categories";
 // import Footer from "../components/Footer";
 // import Navbar from "../components/Navbar";
 // import Newsletter from "../components/Newsletter";
 import Products from "../components/Products/Products";
-import Slider from "../components/Slider/Slider";
+// import Slider from "../components/Slider/Slider";
 
 const Home = () => {
   return (

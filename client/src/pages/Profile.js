0e9587bc9b10d17031d74a1fcd641d71f582d234
Saptak10 <saptak.bugatti20@gmail.com>
2023-01-04 19:15:04 +0530

    bug fix

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -19,6 +19,7 @@ import { getUserDetails, reset } from '../reducers/profile/userProfileSlice'
 
 import '../css/Profile.css'
 import { Button } from '@mui/material';
+import Order from './Order';
 
 function Profile() {
   
@@ -247,41 +248,7 @@ function Profile() {
           </Item>
         </Grid>
         <Grid item xs={8}>
-          <Item>
-          <h2>
-          Your order details
-        </h2>
-          <TableContainer component={Paper}>
-      <Table sx={{ minWidth: 650 }} aria-label="simple table">
-        <TableHead>
-          <TableRow>
-            <TableCell>ID</TableCell>
-            <TableCell align="right">DATE</TableCell>
-            <TableCell align="right">TOTAL</TableCell>
-            <TableCell align="right">PAID</TableCell>
-            <TableCell align="right">DELIVERED</TableCell>
-            <TableCell align="right">ACTIONS</TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {rows.map((row) => (
-            <TableRow
-              key={row.name}
-              sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
-            >
-              <TableCell component="th" scope="row">
-                {row.name}
-              </TableCell>
-              <TableCell align="right">{row.calories}</TableCell>
-              <TableCell align="right">{row.fat}</TableCell>
-              <TableCell align="right">{row.carbs}</TableCell>
-              <TableCell align="right">{row.protein}</TableCell>
-            </TableRow>
-          ))}
-        </TableBody>
-      </Table>
-    </TableContainer>
-          </Item>
+          <Order />
         </Grid>
       </Grid>
     </Box>
